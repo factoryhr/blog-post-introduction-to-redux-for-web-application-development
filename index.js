d20f50e96db628d775ec1973a363a2c7d2596ead
factoryhr <ured@plavatvornica.com>
2018-03-26 15:15:41 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+/** Library **/
+
 function createStore(reducer, preloadedState) {
   if (typeof reducer !== 'function') {
     throw new Error('Expected `reducer` to be a function.');
@@ -5,7 +7,7 @@ function createStore(reducer, preloadedState) {
 
   if (typeof preloadedState === 'function') {
     console.warn(
-      'Received function as `preloadedState`. ' +
+      'Received function as `preloadedState`.' +
       'State is undefined.'
     );
     preloadedState = undefined;
@@ -16,6 +18,18 @@ function createStore(reducer, preloadedState) {
 
   function getState() {
     return currentState;
+  }
+
+  function dispatch(action) {
+    if (action.type === undefined) {
+      throw new Error('Expected `action` to have a `type` property.');
+    }
+    
+    currentState = reducer(currentState, action);
+    
+    for (var i = 0; i < listeners.length; i++) {
+      listeners[i]();
+    }
   }
 
   function subscribe(listener) {
@@ -31,23 +45,83 @@ function createStore(reducer, preloadedState) {
     };
   }
 
-  function dispatch(action) {
-    if (action.type === undefined) {
-      throw new Error('Expected `action` to have a `type` property.');
-    }
-    
-    currentState = reducer(currentState, action);
-    
-    for (var i = 0; i < listeners.length; i++) {
-      listeners[i]();
-    }
-  }
-
   dispatch({ type: '__INIT__' });
 
   return {
     getState: getState,
-    subscribe: subscribe,
     dispatch: dispatch,
+    subscribe: subscribe
   };
 }
+
+/** Case **/
+
+var initialState = {
+  counter: 0,
+};
+
+var actionTypes = {
+  increment: 'increment',
+  decrement: 'decrement',
+};
+
+function incrementAction() {
+  return {
+    type: actionTypes.increment
+  };
+}
+
+function decrementAction() {
+  return {
+    type: actionTypes.decrement
+  };
+}
+
+function incrementHandler(state, action) {
+  var nextState = Object.assign({}, state);
+  nextState.counter += 1;
+  return nextState;
+}
+
+function decrementHandler(state, action) {
+  var nextState = Object.assign({}, state);
+  nextState.counter -= 1;
+  return nextState;
+}
+
+function counterReducer(state, action) {
+  switch (action.type) {
+    case actionTypes.increment:
+      return incrementHandler(state, action);
+    case actionTypes.decrement:
+      return decrementHandler(state, action);
+    default:
+      return state;
+  }
+}
+
+/** Test **/
+
+var buttonDecrement = document.getElementById('counter-decrement');
+var buttonIncrement = document.getElementById('counter-increment');
+var spanValue = document.getElementById('counter-value');
+
+function setSpanValue(value) {
+  spanValue.innerHTML = value;
+}
+
+var store = createStore(counterReducer, initialState);
+
+setSpanValue(store.getState().counter);
+
+store.subscribe(function () {
+  setSpanValue(store.getState().counter);
+});
+
+buttonIncrement.addEventListener('click', function () {
+  store.dispatch(incrementAction());
+});
+
+buttonDecrement.addEventListener('click', function () {
+  store.dispatch(decrementAction());
+});
